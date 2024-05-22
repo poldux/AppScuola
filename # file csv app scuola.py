81ca0177ec3baad76501cd31af1b09a1f620ba54
poldux <150134428+poldux@users.noreply.github.com>
2024-05-22 17:09:49 +0200

    add function add newProf
    aggiunta funzione per aggiungere un dizionario alla lista di dizionari (da completare/errore da correggere)

--- # file csv app scuola.py
+++ # file csv app scuola.py
@@ -1,20 +1,29 @@
 # file csv app scuola 
-
 import csv
-ProfDictionary={}
+from csv import DictWriter
 
+profDictionary={}
 
 fileIn=open("C:\\Users\\giacomopoldi\\Desktop\\prova.csv", "r", encoding="utf-8")
 reader = csv.DictReader(fileIn)
+
 dati = []
+
 for prova in reader:
     #print(prova)
     dati.append(prova)
 fileIn.close()
 
+profDictionary['Professori'] = dati
+print(profDictionary)
 
+field_names = ['Nome', 'Cognome', 'ClasseProf']
+newProf = {'Nome': 'Samuele', 'Cognome': 'Maranghi', 'ClasseProf': '4I1'}
 
-ProfDictionary['Professori'] = dati
-print(ProfDictionary)
+with open('prova.csv', 'a') as f_object:
+    dictwriter_oggetto = DictWriter(f_object, fieldnames=field_names)
+    dictwriter_oggetto.writerow(newProf)
+    f_object.close()
 
-
+print("")
+print(profDictionary)
